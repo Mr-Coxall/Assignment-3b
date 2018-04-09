dd0334a979b946c966c9d03196caa1a8779115f3
GavinZ0316 <31775654+GavinZ0316@users.noreply.github.com>
2018-04-09 13:41:15 -0400

    Update BinarySearchT.java

--- BinarySearchT.java
+++ BinarySearchT.java
@@ -12,39 +12,61 @@ import java.util.List;
 import java.util.Random;
 
  public class BinarySearchT{
-     public static int binarySearch(List<Integer>inputArray, int inputNum){
+ 
+     public boolean binarySearch(List<Integer>inputArray, int inputNum){
          int startP = 0;
          int endP = (inputArray.size()-1);
          int midValue;
-
-         while(startP<endP){
+         while(startP<=endP){
              int midP = (endP + startP)/2;
              midValue = inputArray.get(midP);
 
              if(inputNum == midValue){
-                 return midP;
+                 return true;
              }else if (inputNum < midValue){
                  startP = midP + 1;
              }else if (inputNum > midValue){
                  startP = midP - 1;
              }
          }
-         return-1;
+         return false;
      }
-     
+
+
+	public static List<Integer> sortList(List<Integer> inputArray){
+
+	  List<Integer> sortedList = new ArrayList<>();
+	  Integer smallestNum = 0; 
+			
+	  for(int count = 0 ; count<inputArray.size() ; ) {
+				
+	  smallestNum = inputArray.get(0); 
+		for(int eachNum: inputArray) {
+			smallestNum = (eachNum < smallestNum)? eachNum : smallestNum;
+		}
+				
+		sortedList.add(smallestNum); 
+		inputArray.remove(smallestNum); 
+	}
+			
+	return sortedList;
+			
+		
+}
           
      public static void main(String[] args) {
          Scanner scanner = new Scanner(System.in);
          String input = "";
          int inputNum = 0;
          int numOfPrintList = 100;
-         List<Integer>randomList = new ArrayList<>();
-
+         List<Integer> randomList = new ArrayList<>(), sortedList = new ArrayList<>();
+		
          System.out.println("List here :");
 
          for(int count = 0; count<numOfPrintList; count++){
-               int numOfRange = new Random().nextInt(200);
+               int numOfRange = new Random().nextInt(999);
              System.out.print(numOfRange+" ");
+             randomList.add(numOfRange);
          } 
          
 	  while (input == ""){
@@ -57,14 +79,19 @@ import java.util.Random;
 			System.out.println("Please Input a number!");
 		        input = "";
 		}
-		}
+	}
 		
-         int answer = binarySearch(randomList,inputNum);
+	  sortedList = sortList(randomList); 
+			
+	  System.out.print("Sorted list:"+sortedList+"\n");
+			
+		
+         int answer = binarySearch(sortList,inputNum);
 
          if(answer == 0){
              System.out.println("Number not in list!");
          }else{
-             System.out.println(inputNum+" is at index "+(answer+2) +" from the list");
+             System.out.println(inputNum+" is at index "+(answer) +" from the list");
          }
          
 
